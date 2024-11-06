80015f5c7a53446d450df5d95c1777f19e7d782d
Ruben <rubenfds@icloud.com>
2024-11-06 12:43:16 +0200

    added the menu that will be used and called back to functions

--- scheduler.py
+++ scheduler.py
@@ -8,10 +8,10 @@ class Patient:
         self.priority = priority_level
         
     def patientInfo(self):
-      return f'Patient: {self.name} {self.surname}, ID: {self.ID_num}, Priority Level: {self.priority}'
+      return f"Patient: {self.name} {self.surname}, ID: {self.ID_num}, Priority Level: {self.priority}"
 
 # Test this function with my own details to see if it prints correctly
-# p1 = Patient('ruben', 'da silva', 9907065112085, 5)
+# p1 = Patient("ruben", "da silva", 9907065112085, 5)
 # print(p1.patientInfo())
 
 class Scheduler:
@@ -28,11 +28,11 @@ class Scheduler:
         self.queue.printWaitingList()
                             
     def savePatientToFile(self, patient, status):
-        with open('patients.txt','a') as file:
-            file.write(f'Patient: {patient} >> Status: {status}\n')
+        with open("patients.txt","a") as file:
+            file.write(f"Patient: {patient} >> Status: {status}\n")
     
     def readConsultFile(self):
-        with open('patients.txt','r') as file:
+        with open("patients.txt","r") as file:
             for line in file:
                 print()
                 
@@ -52,3 +52,44 @@ class PriorityQueue:
     def printWaitingList(self):
         for priority, patient in sorted(self.patients_queue, reverse=True):
                     print(patient)
+                    
+def main():
+    scheduler = Scheduler()
+    
+    while True:
+        print("1. Add a new patient")
+        print("2. Retrieve the next patient in line")
+        print("3. Print out the current waiting list")
+        print("4. Save this patient's record")
+        print("5. Print out all patient records")
+        print("6. Close the programme")
+        
+        choice = input("Select one of the following options: ")
+        
+        if choice == "1":
+            name = input("Enter the patient's name: ")
+            surname = input("Enter the patient's surname: ")
+            ID_num = input("Enter the patient's ID number: ")
+            priority = int(input("Enter the priority status from 1-5 with 5 being highest priority: "))
+            patient = Patient(name, surname, ID_num, priority)
+            scheduler.add_patient(patient)
+        elif choice == '2':
+            patient = scheduler.retrieveNextPatient()
+            if patient:
+                print(f"The next patient is: {patient}")
+            else:
+                print("There are no patients in the queue.")
+        elif choice == '3':
+            scheduler.printWaitingList()
+        elif choice == '4':
+            patient = scheduler.retrieveNextPatient()
+            if patient:
+                status = input(f"Enter the status for {patient}: ")
+                scheduler.savePatientToFile(patient, status)
+        elif choice == '5':
+            scheduler.readConsultFile()
+        elif choice == '6':
+            break
+
+if __name__ == "__main__":
+    main()
